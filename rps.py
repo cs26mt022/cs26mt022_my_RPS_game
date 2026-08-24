25ae7d9c8ecc64778b6074ec060318655a9348f2
rakesh-suru <rakeshsuru1990@gmail.com>
2026-08-24 20:53:50 +0530

    Add winner and tie logic

--- rps.py
+++ rps.py
@@ -18,6 +18,17 @@ def main():
     print("You chose:", player_choice)
     print("Computer chose:", computer_choice)
 
+    if player_choice == computer_choice:
+        print("It's a tie!")
+    elif (
+        (player_choice == "rock" and computer_choice == "scissors")
+        or (player_choice == "scissors" and computer_choice == "paper")
+        or (player_choice == "paper" and computer_choice == "rock")
+    ):
+        print("You win!")
+    else:
+        print("Computer wins!")
+
 
 if __name__ == "__main__":
     main()
