d737c486e279cb86ccaaf83cc85fa6ba9601bced
rakesh-suru <rakeshsuru1990@gmail.com>
2026-08-24 20:51:37 +0530

    Add game rules

--- rps.py
+++ rps.py
@@ -1,5 +1,10 @@
 def main():
     print("Welcome to Rock Paper Scissors!")
+    print("\nRules:")
+    print("Rock beats scissors.")
+    print("Scissors beats paper.")
+    print("Paper beats rock.")
+    print("If both players choose the same option, it is a tie.")
 
 
 if __name__ == "__main__":
